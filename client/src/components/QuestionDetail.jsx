e009fbd6d938a1e7857497d6f66dd003372a4893
Joseph Miller <josephmiller@Josephs-MBP.attlocal.net>
2021-12-08 22:47:59 -0500

    done

--- client/src/components/QuestionDetail.jsx
+++ client/src/components/QuestionDetail.jsx
@@ -2,13 +2,15 @@ import {useState, useEffect} from 'react'
 import { useParams, useNavigate } from 'react-router-dom'
 import { getQuestion } from '../services'
 import AnswerForm from './AnswerForm'
+// import { useRef } from "react"
 
 
-export default function QuestionDetail({setQuestion, setToggle, question, answers}) {
+export default function QuestionDetail({ setQuestion, setToggle, question, answers }) {
 
   const [questionAnswers, setQuestionAnswers] = useState([])
   const { id } = useParams()
   const navigate = useNavigate()
+  // const ServicesRef = useRef(null)
   
   
   
@@ -32,28 +34,33 @@ export default function QuestionDetail({setQuestion, setToggle, question, answer
   
 
   const handleClick = () => {
-    navigate(<AnswerForm />)
-  }
+    window.scroll({
+      top: document.body.offsetHeight,
+      left: 0, 
+      behavior: 'smooth',
+  }); 
+}
 
-  return (
-    <div>
-    <div className="question-detail">
-      <h3 className="single">{question.fields.question}</h3>
-        <h4 className="single">By: {question.fields.author}</h4>
-      </div>
-      <button onClick={handleClick}>Answer!</button>
-      <h2 className="Answertitle">Responses</h2>
-      {questionAnswers.map((answer) => (
+    return (
+      <div>
+        <div className="question-detail">
+          <h3 className="single">{question.fields.question}</h3>
+          <h4 className="single">By: {question.fields.author}</h4>
+        </div>
+        <button className="scroll" onClick={handleClick}>Answer!</button>
+        <h2 className="Answertitle">Responses</h2>
+        {questionAnswers.map((answer) => (
         
-        <div key={answer.fields.answers} className="answer">
-          <h3 key={answer.author} className="single">{answer.fields.author} said: </h3>
-          <p key={answer.answers} className="single">{answer.fields.answers}</p>
+          <div key={answer.fields.answers} className="answer">
+            <h3 key={answer.author} className="single">{answer.fields.author} said: </h3>
+            <p key={answer.answers} className="single">{answer.fields.answers}</p>
         
           </div>
         
-      ))}
-      <AnswerForm id={id} setToggle={setToggle} />
+        ))}
+        <AnswerForm id={id} setToggle={setToggle} />
       </div>
     
-  )
-}
+    )
+  }
+// }
